53250fc9cd07b82bcc97647fe9b728bdcc67b4cf
tang <tanguy.bonneau@gmail.com>
2021-11-27 14:08:49 +0100

    Add missing library

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -10,3 +10,5 @@ trap 'echo "\"${last_command}\" command failed with exit code $?."' ERR
 apt-get update
 apt-get -y -q install python3-gst-1.0 gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-alsa
 
+python3 -m pip install --trusted-host pypi.org "python-magic==0.4.16"
+
